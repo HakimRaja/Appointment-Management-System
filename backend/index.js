eff7ae590400d32ff6cf3d5bcd9f9b642d133cec
HakimRaja <hakim.raja756@gmail.com>
2025-07-10 15:07:31 +0500

    config(sequelize):connect database and import dotenv

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,11 @@
-console.log('hello world!');
+require('dotenv').config();
+const sequelize = require('./config/dbConfig.js')
+
+//Connect to database
+sequelize.sync({alter : true})
+.then(()=>{
+    console.log(`Database is connected at post ${process.env.DB_PORT}`)
+})
+.catch((err)=>{
+    console.log(err)
+})
