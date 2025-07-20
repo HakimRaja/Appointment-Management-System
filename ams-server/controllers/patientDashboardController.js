0b33635c933ae53e353409c12761370f510530e0
HakimRaja <hakim.raja756@gmail.com>
2025-07-20 19:23:00 +0500

    feat: patients appointments

--- ams-server/controllers/patientDashboardController.js
+++ ams-server/controllers/patientDashboardController.js
@@ -1,5 +1,6 @@
 const sequelize = require("../config/dbConfig");
 const getYearsDifference = require("../services/patientDashboard");
+const {v4 : uuidv4} = require('uuid');
 
 const getDoctorsList = async (req,res) => {
     try {
@@ -61,18 +62,72 @@ const bookAppointment = async(req,res) =>{
             res.status(400).send({message : 'Something went wrong!'})
         }
         const availability_id = req.body.availability_id;
-        const updateAvailabilities = sequelize.query('UPDATE availabilities set is_booked=:is_booked,updatedAt=NOW() WHERE availability_id = :availability_id',{
-            replacements : {is_booked : true,availability_id},
-            type : sequelize.QueryTypes.UPDATE
-        });
-        const addAppointment = sequelize.query('INSERT INTO appointments(appointment_id,availability_id,user_id,status,createdAt,updatedAt) Values(:appointment_id,:availability_id,:user_id,:status,NOW(),NOW()) RETURNING appointment_id',{
+        
+        const appointment_id = uuidv4();
+        const addAppointment = await sequelize.query('INSERT INTO appointments(appointment_id,availability_id,user_id,"createdAt","updatedAt") Values(:appointment_id,:availability_id,:user_id,NOW(),NOW()) RETURNING appointment_id',{
             replacements : {appointment_id,availability_id,user_id : req.user.user_id},
             type : sequelize.QueryTypes.INSERT
         });
+        const updateAvailabilities = await sequelize.query('UPDATE availabilities set is_booked=:is_booked,"updatedAt"=NOW() WHERE availability_id = :availability_id',{
+            replacements : {is_booked : true,availability_id},
+            type : sequelize.QueryTypes.UPDATE
+        });
+
         return res.status(200).send({message : 'Appointment created successfully'});
     } catch (error) {
+        console.log(error);
         return res.status(500).send({error : error.message})
     }
 }
 
-module.exports = getDoctorsList;
+const getAppointments = async(req,res) =>{
+    try {
+        const patient_id = req.params.id;
+        const appointmentList = await sequelize.query(`Select appointments.appointment_id,appointments.status,availabilities.date,availabilities.start_time,availabilities.end_time,users.name,users.email,specializations.title
+            FROM appointments
+            JOIN availabilities ON appointments.availability_id = availabilities.availability_id
+            JOIN users ON availabilities.user_id = users.user_id
+            JOIN doctors_specializations on users.user_id = doctors_specializations.user_id
+            JOIN specializations on doctors_specializations.specialization_id = specializations.specialization_id
+            WHERE appointments.user_id = :user_id AND appointments."deletedAt" IS NULL AND appointments.status=:status`,{
+            replacements : {user_id : patient_id , status : 'scheduled'},
+            type : sequelize.QueryTypes.SELECT
+        });
+        if (appointmentList.length === 0) {
+            return res.status(200).send(false);
+        }
+        const appointmentInfo = {};
+        appointmentList.forEach((list)=>{
+            const appointment_id = list.appointment_id;
+            if(!appointmentInfo[appointment_id]){
+                appointmentInfo[appointment_id] = {
+                    name : list.name,
+                    appointment_id : list.appointment_id,
+                    status : list.status,
+                    date : list.date,
+                    start_time : list.start_time,
+                    end_time : list.end_time,
+                    email : list.email,
+                    specializations : new Set()
+                }
+            }
+            appointmentInfo[appointment_id].specializations.add(list.title)
+        });
+        const finalAppointments = Object.values(appointmentInfo).map(info => ({...info,specializations : Array.from(info.specializations) }));
+
+        return res.status(200).send({finalAppointments});
+    } catch (error) {
+        console.log(error);
+        return res.status(500).send({error : error.message})
+    }
+}
+
+module.exports = {getDoctorsList,bookAppointment,getAppointments};
+
+/* `Select appointments.appointment_id,appointments.status,availabilities.date,availabilities.start_time,availabilities.end_time,users.name,users.email,specializations.title
+            FROM appointments
+            JOIN availabilities ON appointments.availability_id = availabilities.availability_id
+            JOIN users ON availabilities.user_id = users.user_id
+            JOIN doctors_specializations on users.user_id = doctors_specializations.user_id
+            JOIN specializations on doctors_specializations.specialization_id = specializations.specialization_id
+            WHERE appointments.user_id = 'f6dcbdbf-aaad-4cea-8350-45570445d740' AND appointments."deletedAt" is null AND appointments.status='scheduled'` */
--- ams-server/routes/patientDashboardRoutes.js
+++ ams-server/routes/patientDashboardRoutes.js
@@ -1,10 +1,11 @@
-const getDoctorsList = require('../controllers/patientDashboardController');
+const {getDoctorsList , bookAppointment, getAppointments} = require('../controllers/patientDashboardController');
 const { verify } = require('../middlewares/authMiddleware');
 
 const patientDashboardRouter = require('express').Router();
 
 
 patientDashboardRouter.get('/doctors',verify,getDoctorsList);
-patientDashboardRouter.get('/book',verify,getDoctorsList);
+patientDashboardRouter.post('/book',verify,bookAppointment);
+patientDashboardRouter.get('/appointments/:id',verify,getAppointments);
 
 module.exports = patientDashboardRouter;
