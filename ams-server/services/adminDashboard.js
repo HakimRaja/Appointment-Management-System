5743d91ae90df505bd6d625f75765fe31f84448b
HakimRaja <hakim.raja756@gmail.com>
2025-07-28 20:19:15 +0500

    add: get all users for the admin

--- ams-server/services/adminDashboard.js
+++ ams-server/services/adminDashboard.js
@@ -4,7 +4,7 @@ const getUsers = async (admin_id) => {
     try {
         const users = await sequelize.query(`SELECT u.user_id,u.name,u.email,p.phone_number,u.role,u.is_validated
             FROM users u
-            JOIN phones p ON u.user_id = p.user_id
+            LEFT JOIN phones p ON u.user_id = p.user_id
             WHERE user_id != :admin_id`,{
             replacements : {admin_id},
             type : sequelize.QueryTypes.SELECT
