933ad47b1ad4d20c1f69cfffefddd1e35286fb75
HakimRaja <hakim.raja756@gmail.com>
2025-07-28 16:36:16 +0500

    add: admin dashbboard route,controller and serivce for getting all users

--- ams-server/routes/adminDashboardRoutes.js
+++ ams-server/routes/adminDashboardRoutes.js
@@ -1,10 +1,12 @@
-const { addNonValidatedUser, getNonValidatedUsers, RemoveNonValidatedUser } = require('../controllers/adminDashboardController');
+const { addNonValidatedUser, getNonValidatedUsers, RemoveNonValidatedUser, getAllUsers } = require('../controllers/adminDashboardController');
 const {checkPostRequestBody, checkGetRequestBody} = require('../middlewares/adminDashboarMiddleware');
 const { verify } = require('../middlewares/authMiddleware');
 
 const adminDashboardRouter = require('express').Router();
 
+adminDashboardRouter.get('/users',verify,getAllUsers);
 adminDashboardRouter.get('/getnonvalidatedusers',verify,checkGetRequestBody,getNonValidatedUsers);
 adminDashboardRouter.patch('/addnonvalidated',verify,checkPostRequestBody,addNonValidatedUser);
 adminDashboardRouter.patch('/removefromnonvalidated',verify,checkPostRequestBody,RemoveNonValidatedUser);
+
 module.exports = adminDashboardRouter;
