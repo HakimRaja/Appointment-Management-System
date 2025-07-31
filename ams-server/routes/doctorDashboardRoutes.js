d68e2363626d77d6fbd8b80db0ebeaa7774d5734
HakimRaja <hakim.raja756@gmail.com>
2025-07-31 18:52:10 +0500

    add : route for doctors pagenation

--- ams-server/routes/doctorDashboardRoutes.js
+++ ams-server/routes/doctorDashboardRoutes.js
@@ -1,9 +1,10 @@
-const { getAvailabilities, addAvailability, deleteAvailability, cancelAppointmentAndRemoveAvailability, patientDetails, addAppointmentToComplete } = require('../controllers/doctorDashboardController');
+const { getAvailabilities, addAvailability, deleteAvailability, cancelAppointmentAndRemoveAvailability, patientDetails, addAppointmentToComplete, getAllAvailabilities } = require('../controllers/doctorDashboardController');
 const { verify } = require('../middlewares/authMiddleware');
 const doctorDashboardRouter = require('express').Router();
 
 
 doctorDashboardRouter.get('/availabilities',verify,getAvailabilities);
+doctorDashboardRouter.get('/availabilities/all',verify,getAllAvailabilities);
 doctorDashboardRouter.post('/availabilities',verify,addAvailability);
 doctorDashboardRouter.delete('/availability/:availability_id',verify,deleteAvailability);
 doctorDashboardRouter.delete('/appointment/:availability_id',verify,cancelAppointmentAndRemoveAvailability);
